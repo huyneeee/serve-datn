f36191e75c50c5b3d0b5c4c980ede8218e4f84b4
huyneeee <nqh.one.of.a.kind@gmail.com>
2022-01-07 23:10:48 +0700

    fix deploy

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,24 +1,31 @@
 <?php
+
 namespace App\Providers;
+
 use Illuminate\Support\ServiceProvider;
-//import this
 use Illuminate\Support\Facades\Schema;
-class AppServiceProvider extends ServiceProvider{
-  /**
-  * Register any application services.
-  *
-  * @return void
-  */
-  public function register(){
-     //add this
-     Schema::defaultStringLength(191);
-  }
-  /**
-  * Bootstrap any application services.
-  *
-  * @return void
-  */
-   public function boot(){
-      //
-   }
+use Illuminate\Pagination\Paginator;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Paginator::useBootstrap();
+        Schema::defaultStringLength(191);
+    }
 }
